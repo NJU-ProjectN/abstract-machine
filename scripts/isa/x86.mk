0f6f91dee305ad0b143bf13becc2882eda4d977a
Zihao Yu <yuzihao@ict.ac.cn>
2020-10-11 14:42:32 +0800

    x86: remove endbr32 in Ubuntu 20.04 with a CPU newer than Comet Lake

--- scripts/isa/x86.mk
+++ scripts/isa/x86.mk
@@ -1,3 +1,4 @@
 CFLAGS  += -m32 -fno-pic -fno-omit-frame-pointer -march=i386
+CFLAGS  += -fcf-protection=none # remove endbr32 in Ubuntu 20.04 with a CPU newer than Comet Lake
 ASFLAGS += -m32 -fno-pic
 LDFLAGS += -melf_i386

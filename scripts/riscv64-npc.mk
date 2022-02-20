89939ad7a2b2d7d92d4f72f4db33a48b9d0341eb
Zihao Yu <yuzihao@ict.ac.cn>
2022-02-20 19:17:10 +0800

    riscv64-npc: enable RVM

---
+++ scripts/riscv64-npc.mk
@@ -0,0 +1,22 @@
+include $(AM_HOME)/scripts/isa/riscv64.mk
+
+AM_SRCS := riscv/npc/start.S \
+           riscv/npc/trm.c \
+           riscv/npc/ioe.c \
+           riscv/npc/timer.c \
+           riscv/npc/input.c \
+           riscv/npc/cte.c \
+           riscv/npc/trap.S \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+.PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin

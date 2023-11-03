81ac173949869376d309666696588aee74dbbbae
Zihao Yu <yuzihao@ict.ac.cn>
2023-11-03 16:10:22 +0800

    sciprts,riscv32-nemu: remove 'e' in mabi

--- scripts/riscv32-nemu.mk
+++ scripts/riscv32-nemu.mk
@@ -1,7 +1,7 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-COMMON_CFLAGS += -march=rv32im_zicsr -mabi=ilp32  # overwrite
+COMMON_CFLAGS += -march=rv32im_zicsr -mabi=ilp32   # overwrite
 LDFLAGS       += -melf32lriscv                     # overwrite
 
 AM_SRCS += riscv/nemu/start.S \

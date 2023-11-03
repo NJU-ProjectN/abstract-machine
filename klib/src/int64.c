bcdbc4e249627e7a65eb0de51ec7507c1f9bbaf1
Zihao Yu <yuzihao@ict.ac.cn>
2023-11-03 16:08:46 +0800

    klib,int64: remove unused code
    * they are reported as error in clang

--- klib/src/int64.c
+++ klib/src/int64.c
@@ -231,20 +231,6 @@ typedef union
     }s;
 } utwords;
 
-static __inline ti_int make_ti(di_int h, di_int l) {
-    twords r;
-    r.s.high = h;
-    r.s.low = l;
-    return r.all;
-}
-
-static __inline tu_int make_tu(du_int h, du_int l) {
-    utwords r;
-    r.s.high = h;
-    r.s.low = l;
-    return r.all;
-}
-
 #endif /* CRT_HAS_128BIT */
 
 typedef union

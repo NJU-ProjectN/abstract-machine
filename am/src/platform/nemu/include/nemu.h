1a79bf50576d034838cfc3e39f2af70e4cc1d582
Wenrui Huang <rijuyuezhuhwr@gmail.com>
2023-12-30 13:55:26 +0800

    [FIX] am/src/platform/nemu/include/nemu.h: Add sound buf in VME kernel mapping

--- am/src/platform/nemu/include/nemu.h
+++ am/src/platform/nemu/include/nemu.h
@@ -41,6 +41,7 @@ extern char _pmem_start;
 #define NEMU_PADDR_SPACE \
   RANGE(&_pmem_start, PMEM_END), \
   RANGE(FB_ADDR, FB_ADDR + 0x200000), \
+  RANGE(AUDIO_SBUF_ADDR, AUDIO_SBUF_ADDR + 0x10000), \
   RANGE(MMIO_BASE, MMIO_BASE + 0x1000) /* serial, rtc, screen, keyboard */
 
 typedef uintptr_t PTE;

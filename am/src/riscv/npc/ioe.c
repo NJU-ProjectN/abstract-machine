c383159000a2975dabde43f221deae3d54bc48b0
Zihao Yu <yuzihao@ict.ac.cn>
2024-01-17 13:36:09 +0800

    am,npc: add uart config function

--- am/src/riscv/npc/ioe.c
+++ am/src/riscv/npc/ioe.c
@@ -9,6 +9,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
+static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
@@ -17,6 +18,7 @@ static void *lut[128] = {
   [AM_TIMER_UPTIME] = __am_timer_uptime,
   [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
+  [AM_UART_CONFIG]  = __am_uart_config,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }

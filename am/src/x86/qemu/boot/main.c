81dc48fbba1cd943c0d8fccede015c16f63dd6fd
JmZeroQAQ <1932699628@qq.com>
2024-05-30 11:36:49 +0800

    Fix miscompilation issues caused by strict aliasing rules.

--- am/src/x86/qemu/boot/main.c
+++ am/src/x86/qemu/boot/main.c
@@ -18,8 +18,8 @@ static inline void read_disk(void *buf, int sect) {
   outb(0x1f6, (sect >> 24) | 0xE0);
   outb(0x1f7, 0x20);
   wait_disk();
-  for (int i = 0; i < SECTSIZE / 4; i ++) {
-    ((uint32_t *)buf)[i] = inl(0x1f0);
+  for (int i = 0; i < SECTSIZE / 2; i ++) {
+    ((uint16_t *)buf)[i] = inw(0x1f0);
   }
 }
 
